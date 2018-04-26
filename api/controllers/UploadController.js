4aac30458154056a0560050461b1004142629fac
RumbleFrog <xxlosttechxx@gmail.com>
2018-04-25 23:25:34 -0400

    :construction: Upload Controller

---
+++ api/controllers/UploadController.js
@@ -0,0 +1,19 @@
+/**
+ * UploadController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+    create: async function(req, res) {
+
+    },
+
+    delete: async function(req, res) {
+
+    },
+
+};
+
