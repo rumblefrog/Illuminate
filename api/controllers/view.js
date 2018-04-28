efab5382618f24493cc2d10996d91e43b32bddd4
RumbleFrog <xxlosttechxx@gmail.com>
2018-04-27 20:04:42 -0400

    :construction: View Action

--- api/controllers/view.js
+++ api/controllers/view.js
@@ -29,9 +29,19 @@ module.exports = {
       statusCode: 200
     },
 
+    invalidID: {
+      responseType: "",
+      statusCode: 400
+    },
+
     notFound: {
       responseType: "",
       statusCode: 404
+    },
+
+    fetchFail: {
+      responseType: "",
+      statusCode: 500
     }
 
   },
@@ -39,24 +49,42 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    let err, result, 
+    let err, result, stream, 
     needle = path.parse(inputs.id).name,
     datastore = sails.getDatastore();
 
+    if (!datastore.driver.mongodb.ObjectID.isValid(needle))
+      return exits.invalidID('Invalid ID');
+
     [ err, result ] = await to(
-      datastore
-        .manager
+      datastore.manager
         .collection(sails.config.custom.collection)
         .findOne({
           _id: datastore.driver.mongodb.ObjectID(needle)
         })
     );
 
-    console.log(needle);
-    console.log(err, result);
+    if (err)
+      return exits.fetchFail('Unable to fetch from database');
+
+    if (result === null)
+      return exits.notFound('ID not found');
+    
+    console.log(result);
+
+    stream = await to(
+      sails.config.minio.Minio
+        .getObject(
+          sails.config.minio.settings.bucket,
+          result.object
+        )
+    );
+
+    console.log(stream.Body);
 
     return exits.success();
-
+    
+    // stream.createReadStream().pipe(exits);
   }
 
 
